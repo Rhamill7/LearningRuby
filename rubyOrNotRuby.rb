c59e227f3bb69627580393a8e82225b19a4f9330
Robbie Hamill <robbie.hamill@ckdgalbraith.co.uk>
2016-06-02 12:10:35 +0100

    First Ruby Program!

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -7,3 +7,10 @@ print "Enter Another Value :"
 second_num = gets.to_i
 
 puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+
+# Arithmetic Operators
+puts "6 + 4 = " + (6+4).to_s
+puts "6 - 4 = " + (6-4).to_s
+puts "6 * 4 = " + (6*4).to_s
+puts "6 / 4 = " + (6/4).to_s
+puts "6 % 4 = " + (6%4).to_s
---
+++ rubyOrNotRuby2.rb
@@ -0,0 +1,21 @@
+num_one = 1.000
+
+num99 = 0.999
+
+puts num_one.to_s + " - " + num99.to_s + " = " + 
+(num_one - num99).to_s
+
+big_float = 1.12345678901234
+
+puts (big_float + 0.0000000000005).to_s
+
+puts 1.class
+puts 1.234.class 
+puts "A String".class
+
+A_CONSTANT = 31.4
+
+A_CONSTANT = 1.6
+
+puts A_CONSTANT
+
